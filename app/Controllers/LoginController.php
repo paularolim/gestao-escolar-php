0f60bc5fe1a3ca0d215014fc32bf75b7ff5ddf50
Paula Rolim <rolim.paula1999@gmail.com>
2021-10-27 13:48:28 -0300

    employee - login system

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controllers;
 
+use App\Config\Session\EmployeeSession;
+use App\Models\Employee;
+use Error;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Views\Twig;
@@ -13,5 +16,36 @@ class LoginController
     $view = Twig::fromRequest($request);
 
     return $view->render($response, 'Login/index.html', []);
+  }
+
+  public static function setLogin(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
+  {
+    $view = Twig::fromRequest($request);
+
+    $body = $request->getParsedBody();
+    $type = $body['type'];
+    $document = $body['document'];
+    $password = $body['password'];
+
+    try {
+      if ($type === 'employee') {
+        $employee = Employee::getByDocument($document);
+        if (!$employee instanceof Employee) {
+          throw new Error('User does not exists');
+        }
+        if (!password_verify($password, $employee->getPassword())) {
+          throw new Error('Incorrect password');
+        }
+
+        EmployeeSession::login($employee);
+        header('Location: /dashboard');
+        exit;
+      } else if ($type === 'teacher') {
+      } else if ($type === 'student') {
+      }
+    } catch (Error $error) {
+      die($error);
+      return $view->render($response, 'Login/index.html', ['error' => true]);
+    }
   }
 }
