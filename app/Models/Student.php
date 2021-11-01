cc873630795465ca8f0496630052626c54e1cdb0
Paula Rolim <rolim.paula1999@gmail.com>
2021-10-31 23:14:19 -0300

    refactor student model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -7,6 +7,8 @@ use PDO;
 
 class Student extends Person
 {
+  const TABLE = 'students';
+
   private string $matriculation;
 
   public function __construct()
@@ -22,35 +24,24 @@ class Student extends Person
     // TODO implement here
   }
 
-  public static function login(string $email,  string $password): bool
-  {
-    // TODO implement here
-    return true;
-  }
-  public static function logout(): bool
-  {
-    // TODO implement here
-    return true;
-  }
-
   public static function getAll(array $fileds = ['*'], string $where = null, string $order = null, string $limit = null): array
   {
-    $result = (new Database('students'))->select($fileds, $where, $order, $limit)->fetchAll(PDO::FETCH_ASSOC);
+    $result = (new Database(self::TABLE))->select($fileds, $where, $order, $limit)->fetchAll(PDO::FETCH_ASSOC);
     return $result ? $result : [];
   }
   public static function getById(string $id, array $fileds = ['*']): ?Student
   {
-    $result = (new Database('students'))->select($fileds, 'id = "' . $id . '"')->fetchObject(self::class);
+    $result = (new Database(self::TABLE))->select($fileds, 'id = "' . $id . '"')->fetchObject(self::class);
     return $result ? $result : null;
   }
   public static function getByDocument(string $document, array $fileds = ['*']): ?Student
   {
-    $result = (new Database('students'))->select($fileds, 'document = "' . $document . '"')->fetchObject(self::class);
+    $result = (new Database(self::TABLE))->select($fileds, 'document = "' . $document . '"')->fetchObject(self::class);
     return $result ? $result : null;
   }
   public static function getCount(): int
   {
-    return (int)(new Database('students'))->select(['count(id) as total'])->fetchColumn(0);
+    return (int)(new Database(self::TABLE))->select(['count(id) as total'])->fetchColumn(0);
   }
   public function store(): Student
   {
